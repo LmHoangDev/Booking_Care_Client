c81f71331cd95df7552e2ebbfdfc1c830dfdb755
Hoang Lam <95642674+LmHoangDev@users.noreply.github.com>
2022-03-11 21:08:01 +0700

    ui_banner/changelanguageRedux#47

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,8 +1,22 @@
 import { Component } from "react";
 import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
 import "./HomeHeader.scss";
+import BackGroundKCK from "../../assets/images/133537-khamchuyenkhoa.png";
+import BackGroundKTX from "../../assets/images/133657-khamtuxa.png";
+import BackGroundDVXN from "../../assets/images/133744-dichvuxetnghiem.png";
+import BackGroundKTQ from "../../assets/images/133744-khamtongquat.png";
+import BackGroundSKTT from "../../assets/images/133744-suckhoetinhthan.png";
+import BackGroundGPT from "../../assets/images/134356-goi-phau-thuat.png";
+import BackGroundKTN from "../../assets/images/133744-khamtainha.png";
+import { languages } from "../../utils/constant";
+import { changeLanguageApp } from "../../store/actions";
 class HomeHeader extends Component {
+  changeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+  };
   render() {
+    let language = this.props.language;
     return (
       <>
         <header className="header-home-page">
@@ -16,50 +30,185 @@ class HomeHeader extends Component {
               <div className="col-7 header-center">
                 <div className="header-center-item">
                   <a href="#">
-                    <b>Chuyên khoa</b>
+                    <b>
+                      <FormattedMessage id="homeheader.speciality" />
+                    </b>
                   </a>
                   <br />
                   <a href="#" className="detail">
-                    Tìm bác sĩ theo chuyên khoa
+                    <FormattedMessage id="homeheader.searchdoctor" />
                   </a>
                 </div>
                 <div className="header-center-item">
                   <a href="#">
-                    <b>Cơ sở y tế</b>
+                    <b>
+                      <FormattedMessage id="homeheader.heath-facility" />
+                    </b>
                   </a>
                   <br />
                   <a href="#" className="detail">
-                    Chọn bệnh viện phòng khám
+                    <FormattedMessage id="homeheader.select-room" />
                   </a>
                 </div>
                 <div className="header-center-item">
                   <a href="#">
-                    <b>Bác sĩ</b>
+                    <b>
+                      {" "}
+                      <FormattedMessage id="homeheader.doctor" />
+                    </b>
                   </a>
                   <br />
                   <a href="#" className="detail">
-                    Chọn bác sĩ giỏi
+                    <FormattedMessage id="homeheader.seclect-doctor" />
                   </a>
                 </div>
                 <div className="header-center-item">
                   <a href="#">
-                    <b>Gói khám</b>
+                    <b>
+                      {" "}
+                      <FormattedMessage id="homeheader.fee" />
+                    </b>
                   </a>
                   <br />
                   <a href="#" className="detail">
-                    Khám sức khỏe tổng quát
+                    <FormattedMessage id="homeheader.check-health" />
                   </a>
                 </div>
               </div>
               <div className="col-2 header-right text-left">
-                <a href="#" className="detail">
-                  <i className="fa fa-question-circle"></i>{" "}
-                  <span style={{ color: "#969495" }}>Hỗ trợ</span>
-                </a>
+                <div>
+                  {" "}
+                  <a href="#" className="detail">
+                    <i className="fa fa-question-circle"></i>{" "}
+                    <span style={{ color: "#969495" }}>
+                      {" "}
+                      <FormattedMessage id="homeheader.support" />
+                    </span>
+                  </a>
+                </div>
+                <div className="language">
+                  <div
+                    className={
+                      language === "vi" ? "language-vi active" : "language-vi"
+                    }
+                  >
+                    <span onClick={() => this.changeLanguage(languages.VI)}>
+                      VN
+                    </span>
+                  </div>
+                  <div
+                    className={
+                      language === "en" ? "language-en active" : "language-en"
+                    }
+                  >
+                    <span onClick={() => this.changeLanguage(languages.EN)}>
+                      EN
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </header>
+        <section className="header-home-banner">
+          <h1 className="banner-title text-center text-light py-5">
+            {" "}
+            <FormattedMessage id="banner.title1" /> <br />{" "}
+            <b>
+              {" "}
+              <FormattedMessage id="banner.title2" />
+            </b>
+          </h1>
+          <div className="banner-search text-center">
+            <i className="fa fa-search"></i>
+            <input type="text" placeholder="Search" />
+          </div>
+          <div className="banner-options">
+            <ul className="mt-5 container">
+              <li>
+                <div
+                  style={{
+                    backgroundImage: `url(${BackGroundKCK})`,
+                  }}
+                ></div>
+
+                <a href="#">
+                  <FormattedMessage id="banner.child1" />
+                </a>
+              </li>
+              <li>
+                <div
+                  style={{
+                    backgroundImage: `url(${BackGroundKTX})`,
+                  }}
+                ></div>
+                <a href="#">
+                  <FormattedMessage id="banner.child2" />
+                </a>
+              </li>
+              <li>
+                <div
+                  style={{
+                    backgroundImage: `url(${BackGroundKTQ})`,
+                  }}
+                ></div>
+                <a href="#">
+                  <FormattedMessage id="banner.child3" />
+                </a>
+              </li>
+              <li>
+                <div
+                  style={{
+                    backgroundImage: `url(${BackGroundDVXN})`,
+                  }}
+                ></div>
+                <a href="#">
+                  <FormattedMessage id="banner.child4" />
+                </a>
+              </li>
+              <li>
+                <div
+                  style={{
+                    backgroundImage: `url(${BackGroundSKTT})`,
+                  }}
+                ></div>
+                <a href="#">
+                  <FormattedMessage id="banner.child5" />
+                </a>
+              </li>
+              <li>
+                <div
+                  style={{
+                    backgroundImage: `url(${BackGroundKCK})`,
+                  }}
+                ></div>
+                <a href="#">
+                  <FormattedMessage id="banner.child6" />
+                </a>
+              </li>
+              <li>
+                <div
+                  style={{
+                    backgroundImage: `url(${BackGroundGPT})`,
+                  }}
+                ></div>
+                <a href="#">
+                  <FormattedMessage id="banner.child7" />
+                </a>
+              </li>
+              <li>
+                <div
+                  style={{
+                    backgroundImage: `url(${BackGroundKTN})`,
+                  }}
+                ></div>
+                <a href="#">
+                  <FormattedMessage id="banner.child8" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
       </>
     );
   }
@@ -68,11 +217,14 @@ class HomeHeader extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -9,6 +9,8 @@ const actionTypes = Object.freeze({
   ADD_USER_SUCCESS: "ADD_USER_SUCCESS",
   USER_LOGIN_SUCCESS: "USER_LOGIN_SUCCESS",
   PROCESS_LOGOUT: "PROCESS_LOGOUT",
+
+  CHANGE_LANGUAGE: "CHANGE_LANGUAGE",
 });
 
 export default actionTypes;
