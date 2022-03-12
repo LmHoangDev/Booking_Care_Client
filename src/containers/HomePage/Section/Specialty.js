fa425cda6210597b32c9b71e7210bd637d65dea7
Hoang Lam <95642674+LmHoangDev@users.noreply.github.com>
2022-03-12 11:33:44 +0700

    customize/slider#49

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -2,6 +2,11 @@ import { Component } from "react";
 import { connect } from "react-redux";
 import { FormattedMessage } from "react-intl";
 import "./Specialty.scss";
+import ImgYHCT from "../../../assets/specialty/121232-y-hoc-co-truyen.jpg";
+import ImgCS from "../../../assets/specialty/121215-cot-song.jpg";
+import ImgBSGD from "../../../assets/specialty/195926-bac-si-gia-dinh.jpg";
+import ImgUB from "../../../assets/specialty/195611-ung-buou.jpg";
+import ImgTHHM from "../../../assets/specialty/200111-tao-hinh-ham-mat.jpg";
 import Slider from "react-slick";
 // Import css files
 import "slick-carousel/slick/slick.css";
@@ -12,31 +17,49 @@ class Specialty extends Component {
       dots: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 4,
       slidesToScroll: 1,
     };
     return (
       <>
         <div className="specialty-container">
           <div className="container">
+            <div className="specialty-title d-flex">
+              <h1>Chuyên khoa phổ biến</h1>
+              <div className="see-more">
+                <a href="#">Xem thêm</a>
+              </div>
+            </div>
             <Slider {...settings}>
-              <div>
-                <h3>1</h3>
+              <div class="specialty-item">
+                <a href="#" className="d-block">
+                  <img src={ImgYHCT} alt="" />
+                  <p>Y học cổ truyền</p>
+                </a>
               </div>
-              <div>
-                <h3>2</h3>
+              <div class="specialty-item">
+                <a href="#" className="d-block">
+                  <img src={ImgCS} alt="" />
+                  <p>Cột sống</p>
+                </a>
               </div>
-              <div>
-                <h3>3</h3>
+              <div class="specialty-item">
+                <a href="#" className="d-block">
+                  <img src={ImgBSGD} alt="" />
+                  <p>Bác sĩ gia đình</p>
+                </a>
               </div>
-              <div>
-                <h3>4</h3>
+              <div class="specialty-item">
+                <a href="#" className="d-block">
+                  <img src={ImgUB} alt="" />
+                  <p>Ung bướu</p>
+                </a>
               </div>
-              <div>
-                <h3>5</h3>
-              </div>
-              <div>
-                <h3>6</h3>
+              <div class="specialty-item">
+                <a href="#" className="d-block">
+                  <img src={ImgTHHM} alt="" />
+                  <p>Tạo hình hàm mặt</p>
+                </a>
               </div>
             </Slider>
           </div>
