932839b1e5caecc2eaec3efdd7bef951cb0bdd08
Hoang Lam <95642674+LmHoangDev@users.noreply.github.com>
2022-03-21 22:26:03 +0700

    detailSpecialtyUI#93

--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -4,12 +4,17 @@ import { connect } from "react-redux";
 import { languages } from "../../../utils/constant";
 // import { Redirect, Route, Switch } from "react-router-dom";
 import HomeHeader from "../../HomePage/HomeHeader";
+import DoctorExtraInfor from "../Doctor/DoctorExtraInfor";
+import DoctorSchedule from "../Doctor/DoctorSchedule";
+import ProfileDoctor from "../Doctor/ProfileDoctor";
 import "./DetailSpecialty.scss";
 
 class DetailSpecialty extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      id: 9,
+    };
   }
   async componentDidMount() {}
 
@@ -19,7 +24,65 @@ class DetailSpecialty extends Component {
     return (
       <>
         <HomeHeader isShowBanner={false} />
-        <div className="container">DetailSpecialty</div>
+        <div>Detail specialty</div>
+        <div className="detail-specialty-container">
+          <div className="container">
+            <div className="row detail-specialty-doctor">
+              <div className="col-6 d-flex justify-content-center align-items-start">
+                {" "}
+                <ProfileDoctor
+                  doctorId={this.state.id}
+                  isShowDescription={true}
+                  // dataTime={dataTime}
+                />
+              </div>
+              <div className="col-6">
+                <div className="pt-4">
+                  <DoctorSchedule doctorIdFromParent={this.state.id} />
+                </div>
+                <div>
+                  <DoctorExtraInfor doctorIdFromParent={this.state.id} />
+                </div>
+              </div>
+            </div>
+            <div className="row detail-specialty-doctor">
+              <div className="col-6 d-flex justify-content-center align-items-start">
+                {" "}
+                <ProfileDoctor
+                  doctorId={this.state.id}
+                  isShowDescription={true}
+                  // dataTime={dataTime}
+                />
+              </div>
+              <div className="col-6">
+                <div className="pt-4">
+                  <DoctorSchedule doctorIdFromParent={this.state.id} />
+                </div>
+                <div>
+                  <DoctorExtraInfor doctorIdFromParent={this.state.id} />
+                </div>
+              </div>
+            </div>
+            <div className="row detail-specialty-doctor">
+              <div className="col-6 d-flex justify-content-center align-items-start">
+                {" "}
+                <ProfileDoctor
+                  doctorId={this.state.id}
+                  isShowDescription={true}
+                  // dataTime={dataTime}
+                />
+              </div>
+              <div className="col-6">
+                <div className="pt-4">
+                  <DoctorSchedule doctorIdFromParent={this.state.id} />
+                </div>
+                <div>
+                  <DoctorExtraInfor doctorIdFromParent={this.state.id} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
